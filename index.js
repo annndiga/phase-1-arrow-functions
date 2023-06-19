cda2e7d842bfd1aca8c4f16eea0d71bc0eaab74f
Ann Ndiga <annndiga@gmail.com>
2023-06-19 17:44:01 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+const divide = (a, b) => {
+        
+    return 2000/100;
+    };
+    console.log(divide(2000,100));
+
+  const square = (num) => {
+    return num * num;
+  }
+  console.log(square(5));
+
+  const add = (a, b) => {
+    return a + b;
+  }
+  console.log(add(5, 3)); 
